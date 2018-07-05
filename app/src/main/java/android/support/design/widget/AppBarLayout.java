52f33ef0b828cec257206073aa3a629cdd518ed1
Monil Saraswat <monilsaraswat@gmail.com>
2018-07-05 16:10:02 +0530

    Initial commit

---
+++ app/src/main/java/android/support/design/widget/AppBarLayout.java
@@ -0,0 +1,14 @@
+package android.support.design.widget;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by hp 15 on 2/25/2017.
+ */
+public class AppBarLayout extends View {
+    public AppBarLayout(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+}
